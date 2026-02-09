c7a925b29d7589837eb81a567a20ac3fcf0f104e
EduardaLima-09 <maria.alima@sptech.school>
2026-02-09 00:27:29 -0300

    Primeiro commit do projeto Java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Contando números usando laço for!" + "\n");
+
+        for (int i = 1; i <= 50; i++) {
+            System.out.println("Número: " + i);
+        }
+    }
+}
